15f9873b69cc0268c4a197bbfeea0ca13f77b56f
EvanShechtman <48339840+EvanShechtman@users.noreply.github.com>
2019-09-08 22:38:21 -0700

    Concurrent Queues and Maps

---
+++ Concurrency/Concurrent Patterns/ConcurrentQueuesAndMaps/src/es/collections/ConcurrentHashMapParallelPatterrns.java
@@ -0,0 +1,32 @@
+package es.collections;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import es.collections.model.Actor;
+import es.collections.model.Movie;
+import es.collections.model.MovieReader;
+
+public class ConcurrentHashMapParallelPatterrns {
+
+	public static void main(String[] args) {
+
+		ConcurrentHashMap<Actor, Set<Movie>> map = new ConcurrentHashMap<>();
+
+		MovieReader reader = new MovieReader();
+		reader.addActorsToMap(map);
+		
+		System.out.println("# Actors = " + map.size());
+		
+		int maxMoviesForOneActor = map.reduce(10, (actor, movies) -> movies.size(), Integer::max);
+		System.out.println("Max movies for one actor = " + maxMoviesForOneActor);
+		
+		Actor mostSeenActor = 
+				map.search(10, (actor, movies) -> movies.size() == maxMoviesForOneActor ? actor : null);
+		System.out.println("Most seen actor = " + mostSeenActor);
+		
+		int numberOfMoviesReferences = map.reduce(10, (actor, movies) -> movies.size(), Integer::sum);
+		
+		System.out.println("Average movies per actor = " + numberOfMoviesReferences/map.size());
+	}
+}
